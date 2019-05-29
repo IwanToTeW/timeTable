3f790d371bf0dc862b1716df3501f63be0b4fc36
ivantotev <ivantotev_96@abv.bg>
2019-05-29 14:49:06 +0100

    Set an express server and also created a react client

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const objects = require('./objects');
-const fileIO = require('./fileIO');
-
+const objects = require("./objects");
+const fileIO = require("./fileIO");
+const express = require("express");
 
 //  USING PROMISES
 // const jsonData = fileIO.readFile("lessons.csv").then(values => {return values}).catch(err=>console.log(err));
 
 async function getObjects(fileName) {
-    try {
-        let lessons = await fileIO.readFile(fileName);
-        console.log(lessons);
-        return lessons;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    let lessons = await fileIO.readFile(fileName);
+    console.log(lessons);
+    return lessons;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 let lessons = getObjects("lessons.csv");
 console.log(lessons);
 
+// Below goes server code
+const app = express();
+app.get("/", (req, res) => {
+  res.json([{ id: 1, username: "userOne" }, { id: 2, username: "userTwo" }]);
+});
+
+app.listen(3000);
