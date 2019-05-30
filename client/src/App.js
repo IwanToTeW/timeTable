dbe7777f89796249d430dbd39945b4cf69540314
ivantotev <ivantotev_96@abv.bg>
2019-05-30 14:40:39 +0100

    Implemented the front end for routes /login /logout /profile /register and / , with simple navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,44 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  state = { users: [] };
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
 
-  componentDidMount() {
-    fetch("/users")
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+class App extends Component {
+  // state = { users: [] };
+
+  // componentDidMount() {
+  //   fetch("/users")
+  //     .then(res => res.json())
+  //     .then(users => this.setState({ users }));
+  // }
 
   render() {
     return (
-      <div className="App">
-        <h1> Users</h1>
-        <ul>
-          {this.state.users.map(user => (
-            <li key={user.id}> {user.username}</li>
-          ))}
-        </ul>
-      </div>
+      //   <div className="App">
+      //     <h1> Users</h1>
+      //     <ul>
+      //       {this.state.users.map(user => (
+      //         <li key={user.id}> {user.username}</li>
+      //       ))}
+      //     </ul>
+      //   </div>
+
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
