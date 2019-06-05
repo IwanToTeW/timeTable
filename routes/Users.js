30f9f3458a95a5f13c08e48a6d272bf2e18fb0c4
ivantotev <ivantotev_96@abv.bg>
2019-06-05 14:55:08 +0100

    Implemented the backend for routes /register /login /profile

--- routes/Users.js
+++ routes/Users.js
@@ -19,8 +19,82 @@ users.post("/register", (req, res) => {
     password: req.body.password,
     created: todayDate
   };
-  console.log(userData);
+
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (!user) {
+        console.log("THIS IS USER DATA");
+        console.log(userData);
+        const hash = bcrypt.hashSync(userData.password, 10);
+        userData.password = hash;
+        User.create(userData)
+          .then(user => {
+            let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+              expiresIn: 1440
+            });
+            res.json({ token: token });
+          })
+          .catch(err => {
+            res.send("error: " + err);
+          });
+      } else {
+        res.json({ error: "User already exists" });
+      }
+    })
+    .catch(err => {
+      res.send("error: " + err);
+    });
+  // console.log(userData);
   // db work goes here
 });
 
+// Route "/login"
+
+users.post("/login", (req, res) => {
+  // console.log(req.body.email, req.body.password);
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+          expiresIn: 1440
+        });
+        res.json({ token: token });
+      }
+    })
+    .catch(err => {
+      res.send("error: " + err);
+    });
+});
+
+// Route "/profile"
+users.get("/profile", (req, res) => {
+  var decoded = jwt.verify(
+    req.headers["authorization"],
+    process.env.SECRET_KEY
+  );
+
+  User.findOne({
+    where: {
+      id: decoded.id
+    }
+  })
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.send("User does not exist");
+      }
+    })
+    .catch(err => {
+      res.send("error: " + err);
+    });
+});
 module.exports = users;
